f4e15fa3b0074bdfe97c3c872d6f89a3123b809f
brianmuturi2 <brianmuturi2@gmail.com>
2022-10-27 10:16:13 +0300

    feat(location): started on location pickers

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import {ScrollView, View, Text, TextInput, StyleSheet} from 'react-native';
 import {useState} from 'react';
 import {Colors} from '../../constants/colors';
 import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 
 function PlaceForm() {
 
@@ -18,6 +19,7 @@ function PlaceForm() {
                 <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle}/>
             </View>
             <ImagePicker/>
+            <LocationPicker/>
         </ScrollView>
     )
 }
